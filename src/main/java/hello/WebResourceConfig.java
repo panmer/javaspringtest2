f457e8206fd5b15b410debc00bde6c00a473481a
trungvuive <trung150502@gmail.com>
2023-11-16 21:25:41 +0700

    modified web config

--- src/main/java/hello/WebResourceConfig.java
+++ src/main/java/hello/WebResourceConfig.java
@@ -4,19 +4,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 // @EnableWebMvc
 @Configuration
 public class WebResourceConfig implements WebMvcConfigurer {
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS =
+    {
+		"classpath:/resources/",
+        "classpath:/static/",
+		"classpath:/public/"
+    };
+
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/index", "/index.html");
     }
 
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // All resources go to where they should go
-        registry.addResourceHandler("/*.html")
+        registry.addResourceHandler("./*.html", "/**/*.css", "/**/*.html", "/**/*.js", "/**/*.js.map", "/**/*.css.map",
+                                    "/**/*.jsx", "/**/*.png", "/**/*.jpg", "/**/*.ttf", "/**/*.woff", "/**/*.woff2", "/**/*.ico")
                 .setCachePeriod(0)
-                .addResourceLocations("classpath:/client/build/");
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 }
